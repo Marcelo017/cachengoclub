93aa46e2ce3288d4a31a516fc8ae110df28f37b6
Marcelo <marcelogrando017@gmail.com>
2022-07-08 21:41:39 -0300

    prueba

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Wheel from "./utils/Wheel";
-function Inicio(){
-    return(
+function Inicio() {
+    return (
         <div id='main'>
             <div className='header-heading'>
                 <h4>Bienvenidos #THECLUB</h4>
-                <h2><span>CACHENGO</span> FOR<br/>EVER</h2>
-                <p className='details'>Viernes, sábados y domingos a partir de las 20 hs.</p>
+                <h2><span>CACHENGO</span> FOR<br />EVER</h2>
+                <p className='details'>Viernes, sábados y domingos a partir de las 25 hs.</p>
 
             </div>
             <div className='spingame'>
